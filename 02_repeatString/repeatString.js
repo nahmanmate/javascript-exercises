6e6c05e2ac1411e99b54f472473fab638214cdee
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 15:54:10 +1100

    Added error handling and fixed loop logic for values i < =0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,14 @@ const repeatString = function(str, num) {
     for (let i = 1; i < num; ++i) {
         str0 = str0 + str;
     }
+    if (num > 0) {
     return str0;
+    }
+    else if (num < 0) {
+        return "ERROR";
+    }
+    else
+    return "";
 };
 
 // Do not edit below this line
