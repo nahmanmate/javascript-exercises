06ffe65b546b43f7112f632c1a2f2dd3ee38d06f
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 23:54:02 +1100

    Successfully completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isYearDivisibleBy4 = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleBy400 = year % 400 === 0;
+  if (
+    isYearDivisibleBy4 &&
+    (!isCentury || isYearDivisibleBy400)
+) {
+    return true
+}
+else {
+    return false
+}
 
 };
+console.log(leapYears);
+leapYears(2004);
 
 // Do not edit below this line
 module.exports = leapYears;
