7d34dbfa253d70dc12e1013421961fdbfc0ea696
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 16:28:13 +1100

    Successfully completed all tests question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+function reverseString(str) {
+    //split string and return array of characters
+    let splitString = str.split("");
+    //reverse array character order
+    let reverseArray = splitString.reverse();
+    //join array of characters back to string
+    let reverseString = reverseArray.join("")
+    //return reversed string
+    return reverseString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
