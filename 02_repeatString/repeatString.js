e1536479f32b42fdf8392e97e278d7f57b657d3a
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 15:43:20 +1100

    Successful completion of first test in activity 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let str0 = str
+    for (let i = 1; i < num; ++i) {gi
+        str0 = str0 + str;
+    }
+    return str0;
 };
 
 // Do not edit below this line
