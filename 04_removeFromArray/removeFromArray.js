a897003eaf3820fc2693efbca7be6ca3fa5da27f
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 17:01:19 +1100

    Successfully completed challenge 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(passedArray, ...valueToRemove) {
+    const newArray = []
+    passedArray.forEach(items => {
+        if (!valueToRemove.includes(items)) {
+            newArray.push(items)
+        }
+    });
+    return newArray
 };
 
 // Do not edit below this line
