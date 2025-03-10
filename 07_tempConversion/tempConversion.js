2d42d833cb89e67a3d7e1877ceedbd6b9b12b628
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-11 00:33:43 +1100

    Completed problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let celsTemp = (x - 32) * (5/9);
+  let roundedCelsTemp = Math.round(celsTemp *10) / 10
+  return roundedCelsTemp
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  let farenTemp = (y * 9 / 5) + 32;
+  let roundedFarenTemp = Math.round(farenTemp * 10) / 10
+  return roundedFarenTemp;
 };
 
 // Do not edit below this line
