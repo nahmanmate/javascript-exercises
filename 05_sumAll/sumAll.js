b1f69e812bbc7f258b742563c693bf16d93384c5
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-10 23:25:21 +1100

    Successfully completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR"
+    }
+    if (min > max) {
+        //assign temporary value
+        const temp = min;
+        //use temp value to swap min and max
+        min = max;
+        max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
+const sum = sumAll(1,4)
+sumAll(1,4)
+console.log(sum)
 
 // Do not edit below this line
 module.exports = sumAll;
