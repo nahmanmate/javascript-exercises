e75e5148ad83aa186579d201c8cfc91e26d83ae2
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-15 03:28:18 +1100

    partial completion of exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length <1) {
+    return 0;
+  }
+  else if (arr.length < 2) {
+    return arr[0]
+  }
+	return arr.reduce((total,item) => {total + item},0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length <1) {
+    return 0;
+  }
+  else return arr.reduce((total, items) => {total * item}, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return () => {
+    if (a < 0)
+      return -1;
+    else if (a == 0)
+      return 1;
+    else if (a > 0)
+      return (num * factorial(a-1));
+  }
 };
 
 // Do not edit below this line
