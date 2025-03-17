3d59b7d2abd809ea0d06d1ec27ba6647d0385b28
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-17 15:36:18 +1100

    Finished 09 palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    let formattedStr = str.replace(/[^A-Za-z0-9]/g,"");
+    let lowerCaseStr = formattedStr.toLowerCase();
+    let array = lowerCaseStr.split("");
+    // if first matches last, return true
+    let median = 0.5 * array.length
+    // for (i = 1; i < median; i ++) {
+    //     if array[]
+    let ispalindrome = true
+    for (let i = 0; i < median; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                ispalindrome = false
+                break}
+    }
+    return ispalindrome
+}
+console.log(palindromes("rssdqr"));
 // Do not edit below this line
 module.exports = palindromes;
