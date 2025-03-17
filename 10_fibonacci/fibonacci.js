db915e7b4bb71413c5ca100b9334a3f431105225
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-17 16:36:31 +1100

    Finished task 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let int = parseInt(n)
+    if (int < 0) return "OOPS";
+    if (int === 0) return 0;
 
-};
+    let a = 0;
+    let b = 1
+
+    for (let i = 1; i < n; i++) {
+        let temp = b
+        b = a + b
+        a = temp
+    }
+    return b;
+}
+console.log(fibonacci(9))
 
 // Do not edit below this line
 module.exports = fibonacci;
