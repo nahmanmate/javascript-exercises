34a32387fa79262c95d17bf164b011df4bac1347
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-17 13:52:27 +1100

    Completed calculator questions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,3 +1,5 @@
+let testArray = [1, 3, 5, 10]
+
 const add = function(a,b) {
   return a + b
 	
@@ -7,37 +9,31 @@ const subtract = function(a,b) {
 	return a - b
 };
 
-const sum = function(arr) {
-  if (arr.length <1) {
-    return 0;
-  }
-  else if (arr.length < 2) {
-    return arr[0]
-  }
-	return arr.reduce((total,item) => {total + item},0)
+const sum = function(array) {
+  let answer = array.reduce((total, items) => {return total + items},0)
+  return answer
 };
 
-const multiply = function(arr) {
-  if (arr.length <1) {
-    return 0;
-  }
-  else return arr.reduce((total, items) => {total * item}, 1)
+const multiply = function(array) {
+  return array.reduce((total, items) => { return total * items}, 1)
 };
 
 const power = function(a,b) {
   return Math.pow(a, b)
 };
 
-const factorial = function(a) {
-	return () => {
-    if (a < 0)
-      return -1;
-    else if (a == 0)
-      return 1;
-    else if (a > 0)
-      return (num * factorial(a-1));
+const factorial = function(integer) {
+  // integer multiplation of last item by each other item before it
+  if (integer === 0 || integer === 1)
+    return 1;
+  else 
+  for (let i = integer - 1; i >= 1; i--) {
+    integer *= i;
   }
+  return integer
+  // console.log(factorial)
 };
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
